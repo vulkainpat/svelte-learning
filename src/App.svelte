c95c52f46e4a03be0a47dde034927c9603e20dd5
Patrick Dibongue <vulkain808@hotmail.com>
2022-08-31 16:34:23 -0400

    finalise installation

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,24 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
+  let name = "Pat";
+  let age = 46;
+
+  function incrementAge() {
+    age += 1;
+  }
 </script>
 
 <main>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+      <img src="src/assets/vite.svg" class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      <img src="src/assets/logo.svg" class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
+  <h2>Hello {name}, my age is {age}</h2>
+  <button on:click="{incrementAge}">Change Age</button>
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
